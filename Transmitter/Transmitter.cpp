75b4bd0e47591beb67ab6342184a583caf03bb47
WorkingPC <steamboxgame@mail.ru>
2022-08-19 17:04:46 +0300

    working version

--- Transmitter/Transmitter.cpp
+++ Transmitter/Transmitter.cpp
@@ -7,58 +7,95 @@
 #define DEFAULT_DESTINATION_IP		"192.168.28.111"	// ip "Приемника" 
 
 #define DEFAULT_LOCALHOST_UDP_PORT		55553	// Порт для приема данных со станции
-#define DEFAULT_LOCALHOST_TCP_PORT		52423	// Переделать
+#define DEFAULT_LOCALHOST_TCP_PORT		55520	// Переделать
 #define DEFAULT_STATION_UDP_PORT		55552	// Порт станции
-#define DEFAULT_STATION_TCP_PORT		55550   // Порт станции
+#define DEFAULT_STATION_TCP_PORT		55551   // Порт станции
 
 #define DEFAULT_SOURCE_UDP_PORT			55554	// Порт передатчика
 #define DEFAULT_DESTINATION_UDP_PORT	55555	// Порт приемника
 
+#define FREQ							48000
+
+WSADATA wsaData;
+int err1 = WSAStartup(MAKEWORD(2, 2), &wsaData);
 
 int main() {
-	WSADATA wsaData;
-	SOCKET	udp_socket			= INVALID_SOCKET, // Сокет для отправки данных на передатчик
-			udp_station_socket	= INVALID_SOCKET, // Сокет для приема данных со станции
-			tcp_socket			= INVALID_SOCKET; // Сокет для отправки сообщений на станцию
-	u_long	ioMode				= 1;			  // Переменная для переключения режима блокировки сокетов
-	fd_set	master_set			= fd_set();       // Набор рабочих дескрипторов сокетов
-	timeval timeout				= {3 * 60, 0};	  // Таймаут приема данных от станции
+	std::mutex locker;
+	std::vector<std::vector<char>> buffer;
 
-	char	buffer[65535]; // Буфер для данных
-	
-	int		new_desc	= 0, 
-			end_server	= FALSE, 
-			close_conn	= FALSE,
-			err			= WSAStartup(MAKEWORD(2, 2), &wsaData),
-			len			= 0,
-			total_received = 0;
 
-	if (err != 0) {
-		printf("WSAStartup failed with error: %d\n", err);
-		return 1;
-	}
+	std::thread receiver = std::thread(StationReceiver, std::ref(buffer), std::ref(locker));
+	std::thread transmitter = std::thread(Transmitter, std::ref(buffer), std::ref(locker));
 
-	try {
-		udp_socket			= socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-		udp_station_socket	= socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-		tcp_socket			= socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
-		ioctlsocket(udp_station_socket, FIONBIO, &ioMode);
-		ioctlsocket(udp_socket, FIONBIO, &ioMode);
-		ioctlsocket(tcp_socket, FIONBIO, &ioMode);
-	}
-	catch (std::exception e) {
+	receiver.detach();
+	transmitter.detach();
 
-	};
 
-	
-	sockaddr_in udpLocalhost	= SockAddrInit(DEFAULT_LOCALHOST_IP,	DEFAULT_LOCALHOST_UDP_PORT);	// для приема со станции
-	sockaddr_in tcpLocalhost	= SockAddrInit(DEFAULT_LOCALHOST_IP,	DEFAULT_LOCALHOST_TCP_PORT);	// для отправки на станцию
-	sockaddr_in udpStation		= SockAddrInit(DEFAULT_STATION_IP,		DEFAULT_STATION_UDP_PORT);		// для приема со станции
-	sockaddr_in tcpStation		= SockAddrInit(DEFAULT_STATION_IP,		DEFAULT_STATION_TCP_PORT);		// для отправки на станцию
-	sockaddr_in udpSource		= SockAddrInit(DEFAULT_SOURCE_IP,		DEFAULT_SOURCE_UDP_PORT);		// для отправки на приемник
-	sockaddr_in udpRemote		= SockAddrInit(DEFAULT_DESTINATION_IP,	DEFAULT_DESTINATION_UDP_PORT);	// для отправки на приемник
+	char a = 0;
+	do {
+		a = getchar();
+	} while (a != 'Q');
 
+	return 0;
+}
+
+
+int Transmitter(std::vector<std::vector<char>>& buffer, std::mutex& locker) {
+	SOCKET		udp_socket = INVALID_SOCKET;
+	u_long		ioMode = 1;
+	int			err = 0;
+	char		tempBuffer[PACKET_SIZE];
+
+	std::ofstream file("C:\\Users\\Admin\\Desktop\\test2.wav", std::ios::binary);
+	sockaddr_in udpSource = SockAddrInit(DEFAULT_SOURCE_IP, DEFAULT_SOURCE_UDP_PORT);		// для отправки на приемник
+	sockaddr_in udpRemote = SockAddrInit(DEFAULT_DESTINATION_IP, DEFAULT_DESTINATION_UDP_PORT);	// для отправки на приемник
+
+	udp_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+
+	err = bind(udp_socket, (sockaddr*)&udpSource, sizeof(udpSource));
+	IsSocketExcept(err);
+	err = connect(udp_socket, (sockaddr*)&udpRemote, sizeof(udpRemote));
+	IsSocketExcept(err);
+
+	ioctlsocket(udp_socket, FIONBIO, &ioMode);
+
+	int counter = 0;
+	do {
+		while (buffer.size() > 0) {
+			locker.lock();
+			std::copy(buffer[0].begin(), buffer[0].end(), tempBuffer);
+			err = send(udp_socket, tempBuffer, sizeof(tempBuffer), NULL);
+			IsSocketExcept(err);
+			file.write(tempBuffer, sizeof(tempBuffer));
+			buffer.erase(buffer.begin());
+			locker.unlock();
+			Sleep(1000 / 94);
+		}
+	} while (true);
+}
+
+int StationReceiver(std::vector<std::vector<char>>& buffer, std::mutex& locker) {
+	SOCKET		udp_station_socket	= INVALID_SOCKET,
+				tcp_socket			= INVALID_SOCKET;
+	int			err			= 0, 
+				_start		= 0x02,
+				_continue	= 0x03,
+				_pause		= 0x04,
+				_end		= 0x05;
+	u_long		ioMode		= 1;
+	char		signal;
+	bool		is_ended	= false;
+	char tempBuffer[PACKET_SIZE];
+	std::vector<char> tempererBuffer;
+
+	udp_station_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	tcp_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+	sockaddr_in udpLocalhost = SockAddrInit(DEFAULT_LOCALHOST_IP, DEFAULT_LOCALHOST_UDP_PORT);	// udp source
+	sockaddr_in tcpLocalhost = SockAddrInit(DEFAULT_LOCALHOST_IP, DEFAULT_LOCALHOST_TCP_PORT);	// tcp source
+	sockaddr_in udpStation = SockAddrInit(DEFAULT_STATION_IP, DEFAULT_STATION_UDP_PORT);		// udp dest
+	sockaddr_in tcpStation = SockAddrInit(DEFAULT_STATION_IP, DEFAULT_STATION_TCP_PORT);		// tcp dest
 
 	err = bind(tcp_socket, (sockaddr*)&tcpLocalhost, sizeof(tcpLocalhost));
 	IsSocketExcept(err);
@@ -70,63 +107,63 @@ int main() {
 	err = connect(udp_station_socket, (sockaddr*)&udpStation, sizeof(udpStation));
 	IsSocketExcept(err);
 
-	err = bind(udp_socket, (sockaddr*)&udpSource, sizeof(udpSource));
-	IsSocketExcept(err);
-	err = connect(udp_socket, (sockaddr*)&udpRemote, sizeof(udpRemote));
-	IsSocketExcept(err);
+	ioctlsocket(udp_station_socket, FIONBIO, &ioMode);
+	ioctlsocket(tcp_socket, FIONBIO, &ioMode);
 
-	FD_SET(tcp_socket, &master_set);
-	FD_SET(udp_station_socket, &master_set);
+	int rcvbufsz(FREQ * 10), newrcvbufsz(0);
+	int nSizer(sizeof(int));
+	int nretValue(setsockopt(udp_station_socket, SOL_SOCKET, SO_RCVBUF, (char*)(&rcvbufsz), sizeof(int)));
+	if (nretValue != 0)
+	{
+		printf("Error in setsockopt(SO_RCVBUF) (returned value=%d) with errorcode(WSAGetLastError)=%lu.", nretValue, ::WSAGetLastError());
+		return false;
+	}
+
+	signal = GetBytes(_start)[0];
+	err = send(tcp_socket, &signal, 1, NULL);
+
+	int receivedBytes; 
 
 	do {
-		FD_SET(tcp_socket, &master_set);
-		memset(&buffer, 0, sizeof(buffer));
-		for (int i = 0; i <= master_set.fd_count; i++) {
-			err = select(master_set.fd_count, &master_set, NULL, NULL, &timeout);
-			IsSocketExcept(err);
+		while (buffer.size() * PACKET_SIZE < FREQ / 4) {
+			locker.lock();
 
-			if (master_set.fd_array[i] == tcp_socket) {
-				new_desc = accept(tcp_socket, NULL, NULL);
-				if(new_desc>0)
-					FD_SET(new_desc, &master_set);
+			receivedBytes = err = recv(udp_station_socket, tempBuffer, sizeof(tempBuffer), NULL);
+
+			if (err == -1) {
+				signal = GetBytes(_continue)[0];
+				err = send(tcp_socket, &signal, sizeof(signal), NULL);
 			}
-			else {
-				err = recv(master_set.fd_array[i], buffer, sizeof(buffer), 0);
-				if (err < 0)
-				{
-					if (errno != EWOULDBLOCK)
-					{
-						printf("%lu: nothing to receive\n", master_set.fd_array[i]);
-						close_conn = TRUE;
-						FD_CLR(master_set.fd_array[i], &master_set);
-					}
-					continue;
-				}
 
-				if (err == 0)
-				{
-					printf("%lu: Connection closed\n", master_set.fd_array[i]);
-					close_conn = TRUE;
-					FD_CLR(master_set.fd_array[i], &master_set);
-					continue;
-				}
+			if (receivedBytes > 0) {
+				std::cout << WSAGetLastError() << std::endl;
 
-				printf("%lu: %s", master_set.fd_array[i], std::string(buffer, err).c_str());
-				len = err;
-				total_received += len; 
-				printf("\t%d bytes received\n%d in total", len, total_received);
+				tempererBuffer = std::vector<char>(tempBuffer, tempBuffer + sizeof(tempBuffer));
+				buffer.push_back(tempererBuffer);
+			}
+			locker.unlock();
 
-				err = sendto(udp_socket, buffer, err, NULL, (sockaddr*)&udpRemote, sizeof(udpRemote));
-				printf("\t%d bytes sent\n", err);
-				IsSocketExcept(err);
+			if (is_ended && buffer.size() == 0) {
+				closesocket(tcp_socket);
+				closesocket(udp_station_socket);
+				exit(0);
 			}
 		}
+
+		err = recv(tcp_socket, &signal, 1, NULL);
+
+
+		signal = GetBytes(_pause)[0];
+		err = send(tcp_socket, &signal, sizeof(signal), NULL);
+		//std::cout << "send err: " << err << std::endl;
 
 	} while (true);
 }
 
-
-
+template <typename T>
+char* GetBytes(T value) {
+	return static_cast<char*>(static_cast<void*>(&value));
+}
 void IsSocketExcept(int result) {
 	if (result == SOCKET_ERROR) {
 		printf("Socket action failed with error: %d\n", WSAGetLastError());
@@ -165,103 +202,3 @@ T SwapEndian(T u)
 
 	return dest.u;
 }
-
-//do {
-//	memcpy(&working_set, &master_set, sizeof(master_set));
-//	printf("Waiting on select()...\n");
-//	err = select(max_desc + 1, &working_set, NULL, NULL, &timeout);
-//
-//	if (err < 0) {
-//		perror("select() failed.\n");
-//		break;
-//	}
-//	if (err == 0)
-//	{
-//		printf("select() timed out.\n");
-//		break;
-//	}
-//
-//	ready_desc = err;
-//	for (int i = 0; i <= max_desc && ready_desc > 0; ++i) {
-//		if (FD_ISSET(i, &working_set))
-//		{
-//			ready_desc--;
-//			if (i == tcp_socket)
-//			{
-//				printf("Listening socket is readable\n");
-//
-//				do
-//				{
-//					new_desc = accept(tcp_socket, NULL, NULL);
-//					if (new_desc < 0)
-//					{
-//						if (errno != EWOULDBLOCK)
-//						{
-//							perror("accept() failed");
-//							end_server = TRUE;
-//						}
-//						break;
-//					}
-//
-//
-//					printf("  New incoming connection - %d\n", new_desc);
-//					FD_SET(new_desc, &master_set);
-//					if (new_desc > max_desc)
-//						max_desc = new_desc;
-//
-//
-//
-//				} while (new_desc != -1);
-//			}
-//
-//			else
-//			{
-//				printf("  Descriptor %d is readable\n", i);
-//				close_conn = FALSE;
-//				do
-//				{
-//					err = recv(i, buffer, sizeof(buffer), 0);
-//					if (err < 0)
-//					{
-//						if (errno != EWOULDBLOCK)
-//						{
-//							perror("  recv() failed");
-//							close_conn = TRUE;
-//						}
-//						break;
-//					}
-//
-//					if (err == 0)
-//					{
-//						printf("  Connection closed\n");
-//						close_conn = TRUE;
-//						break;
-//					}
-//					len = err;
-//					printf("  %d bytes received\n", len);
-//
-//					err = send(i, buffer, len, 0);
-//					if (err < 0)
-//					{
-//						perror("  send() failed");
-//						close_conn = TRUE;
-//						break;
-//					}
-//
-//				} while (TRUE);
-//
-//
-//				if (close_conn)
-//				{
-//					FD_CLR(i, &master_set);
-//					if (i == max_desc)
-//					{
-//						while (FD_ISSET(max_desc, &master_set) == FALSE)
-//							max_desc -= 1;
-//					}
-//				}
-//			}
-//		}
-//
-//	}
-//} while (end_server == FALSE);
--- Transmitter/Transmitter.h
+++ Transmitter/Transmitter.h
@@ -3,11 +3,20 @@
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include <vector>
+#include <fstream>
+#include <mutex>
+#include <thread>
+
 #pragma comment(lib, "Ws2_32.lib")
 
+#define PACKET_SIZE	512
 
 int main();
+int StationReceiver(std::vector<std::vector<char>>& buffer, std::mutex& locker);
+int Transmitter(std::vector<std::vector<char>>& buffer, std::mutex& locker);
 sockaddr_in SockAddrInit(std::string ip_addr, u_short port);
 template <typename T>
 T SwapEndian(T u);
+template <typename T>
+char* GetBytes(T value);
 void IsSocketExcept(int result);
